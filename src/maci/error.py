7ed87834bf09204cd2e4ea2363cdae3ba5d828f0
aaronater10 <dev_admin@dunnts.com>
2023-01-07 22:47:04 -0600

    added backwards compatibility support for old sfcparse exception names

--- src/maci/error.py
+++ src/maci/error.py
@@ -52,3 +52,36 @@ class XmlLoad(MaciError): __module__ = 'maci.error'
 class XmlLoadStr(MaciError): __module__ = 'maci.error'
 class XmlDump(MaciError): __module__ = 'maci.error'
 class XmlDumpStr(MaciError): __module__ = 'maci.error'
+
+
+# Name compatibility aliases/deprecation from ported library
+def __getattr__(attr_name: str) -> object:
+    # Native
+    if attr_name == 'ImportFile': return Load
+    if attr_name == 'ImportFileRaw': return LoadRaw
+    if attr_name == 'ImportAttrs': return LoadAttrs
+    if attr_name == 'SaveFile': return Dump
+    if attr_name == 'ExportFile': return DumpRaw
+    if attr_name == 'AppendFile': return DumpRaw
+    # JSON
+    if attr_name == 'JsonImportFile': return JsonLoad
+    if attr_name == 'JsonImportStr': return JsonLoadStr
+    if attr_name == 'JsonExportFile': return JsonDump
+    if attr_name == 'JsonExportStr': return JsonDumpStr
+    # YAML
+    if attr_name == 'YamlImportFile': return YamlLoad
+    if attr_name == 'YamlImportStr': return YamlLoadStr
+    if attr_name == 'YamlExportFile': return YamlDump
+    if attr_name == 'YamlExportStr': return YamlDumpStr
+    # INI
+    if attr_name == 'IniImportFile': return IniLoad
+    if attr_name == 'IniExportFile': return IniDump
+    # XML
+    if attr_name == 'XmlImportFile': return XmlLoad
+    if attr_name == 'XmlImportStr': return XmlLoadStr
+    if attr_name == 'XmlExportFile': return XmlDump
+    if attr_name == 'XmlExportStr': return XmlDumpStr
+
+    # Raise normal error if anything else
+    raise_msg = f"module '{__name__}' has no attribute '{attr_name}'"
+    raise AttributeError(raise_msg)
