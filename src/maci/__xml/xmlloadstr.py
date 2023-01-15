0806cf4d74f00cbf1849ad3d9cb1a11e37b7faf5
aaronater10 <dev_admin@dunnts.com>
2023-01-15 03:11:49 -0600

    updated xml param name to be consistent

--- src/maci/__xml/xmlloadstr.py
+++ src/maci/__xml/xmlloadstr.py
@@ -6,7 +6,7 @@ from ..error import XmlLoadStr
 
 #########################################################################################################
 # Import xml str
-def xmlloadstr(data: str) -> __xml_etree.Element:
+def xmlloadstr(xml_str_data: str) -> __xml_etree.Element:
     """
     Imports xml data from a string
 
@@ -19,9 +19,9 @@ def xmlloadstr(data: str) -> __xml_etree.Element:
     This is using the native xml library via etree shipped with the python standard library.
     For more information on the xml.etree api, visit: https://docs.python.org/3/library/xml.etree.elementtree.html#module-xml.etree.ElementTree
     """
-    __err_msg_str = f"Only str is allowed for data"
+    __err_msg_str = f"Only str is allowed for 'xml_str_data'"
 
-    if not isinstance(data, str): raise XmlLoadStr(__err_msg_str, f'\nDATA: {repr(data)}')
+    if not isinstance(xml_str_data, str): raise XmlLoadStr(__err_msg_str, f'\nDATA: {repr(xml_str_data)}')
     try:
-        return __xml_etree.fromstring(str(data))
-    except __xml_etree.ParseError as __err_msg: raise XmlLoadStr(__err_msg, f'\nDATA: {data}')
+        return __xml_etree.fromstring(str(xml_str_data))
+    except __xml_etree.ParseError as __err_msg: raise XmlLoadStr(__err_msg, f'\nDATA: {xml_str_data}')
