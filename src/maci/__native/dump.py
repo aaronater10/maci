e2db96f6aaedcb118499592442c268d3ce675e21
aaronater10 <dev_admin@dunnts.com>
2023-01-31 02:03:48 -0600

    MAJOR: fix dump not dumping attrs correctly. added attr type filters

--- src/maci/__native/dump.py
+++ src/maci/__native/dump.py
@@ -1,7 +1,6 @@
 # dump
 #########################################################################################################
 # Imports
-from ast import literal_eval as __literal_eval__
 from typing import Union as __Union
 from typing import Any as __Any
 from typing import NewType as __NewType
@@ -23,7 +22,13 @@ def dump(
     write_mode: str='w',
     indent_level: int=1,
     indentation_on: bool=True,
-    verify_if_class_instantiated=True
+    class_attrs=False,
+    private_all_under_attrs=False,
+    private_init_under_attrs=False,
+    private_class_under_attrs=False,
+    private_all_dunder_attrs=False,
+    private_init_dunder_attrs=False,
+    private_class_dunder_attrs=False,
     ) -> None:
     """
     Saves your Attr or Key/Value pair data to a file with the new data.
@@ -62,19 +67,16 @@ def dump(
     __err_msg_type_str_write_mode = "Only str is allowed for write_mode"
     __err_msg_type_int_indent_level = "Only int is allowed for indent_level"
     __err_msg_type_bool_indentation_on = "Only bool is allowed for indentation_on"
-    __err_msg_class_instance_error = "Seeing internal built-in names to save. Normally due to a class not being instantiated. If intentional, turn off check with 'verify_if_class_instantiated = False'"
 
     if not isinstance(filename, str): raise Dump(__err_msg_type_str_filename, f'\nFILE: "{filename}"')
     if not isinstance(write_mode, str): raise Dump(__err_msg_type_str_write_mode, f'\nFILE: "{filename}" \nDATA: {write_mode}')
     if not isinstance(indent_level, int): raise Dump(__err_msg_type_int_indent_level, f'\nFILE: "{filename}" \nDATA: {indent_level}')
     if not isinstance(indentation_on, bool): raise Dump(__err_msg_type_bool_indentation_on, f'\nFILE: "{filename}" \nDATA: {indentation_on}')
 
-
     # Save Data to File
     __build_data_output = ""
     __assignment_operators = ('=', '$=', '==', '$==')
     __skip_object_key = ('_MaciDataObjConstructor', '__maci_file_format_id')
-    __non_instance_names = ['__module__', '__init__']
     __locked_attr_list_key =  '_MaciDataObjConstructor__assignment_locked_attribs'
     __reference_attr_list_key =  '_MaciDataObjConstructor__assignment_reference_attribs'
     __maci_file_format_id_match = "48448910-fa49-45ca-bd3e-38d7af136af5-7bcece52-e5ee-4272-989d-103f07aa6c0f"
@@ -141,21 +143,127 @@ def dump(
         return None
 
 
-    ### CLASS: Check if any Class Object with Attributes
+    ### CUSTOM CLASS: Check if any Class Object with Attributes
     _filter_objects = (str, int, float, bool, list, tuple, set, type(None), bytes, complex, range, frozenset, bytearray, memoryview)
     if not isinstance(data, _filter_objects):
-        # Build Data
-        for key,value in data.__dict__.items():
-            # Verify Class is Instantiated
-            if verify_if_class_instantiated:
-                if key in __non_instance_names:
-                    raise Dump(__err_msg_class_instance_error, f'\nFILE: "{filename}" \nCLASS: {data} \nATTR_NAME: {key}')
-            # Normal Assignment
-            if __multiline_check(value) and indentation_on:
-                value = __cleanformat(value, indent_level)
-                __build_data_output += f'{key} {__assignment_operators[0]} {value}\n'
-            else: __build_data_output += f'{key} {__assignment_operators[0]} {repr(value)}\n'
+        # Setup
+        _init_attr_header = '# attrs: init'
+        __build_data_output_init = ''
+        _class_attr_header = '# attrs: class'
+        __build_data_output_class = ''
+        _is_dunder_attr = False
+        _is_case1_build = False
+        _is_case2_build = False
+        _is_case3_build = False
 
+        # Case 1 - Class Attrs Only. Is Instantiated
+        if (bool(vars(data)) == False) and class_attrs:
+            _is_case1_build = True
+            
+            for key,value in vars(type(data)).items():
+                # Strip Unneeded Attrs/Methods, and Preserve Dunders if Required
+                if callable(value): continue
+                if (key.startswith('__') and key.endswith('__')): continue
+                if key.startswith(f'_{type(data).__name__}'): # dunder detection (name mangle)
+                    if (private_all_dunder_attrs) or (private_class_dunder_attrs):
+                        key = key.removeprefix(f'_{type(data).__name__}')
+                        _is_dunder_attr = True
+                    else: continue
+                if (key.startswith('_')) and not (_is_dunder_attr):
+                    if not (private_all_under_attrs or private_class_under_attrs):
+                        continue
+                _is_dunder_attr = False # reset dunder check for single under's
+
+                # Build Data
+                if __multiline_check(value) and indentation_on:
+                    value = __cleanformat(value, indent_level)
+                    __build_data_output += f'{key} {__assignment_operators[0]} {value}\n'
+                else: __build_data_output += f'{key} {__assignment_operators[0]} {repr(value)}\n'
+
+        # Case 2 - Class Attrs Only. Not Instantiated
+        elif '__mro__' in vars(type(data)) and class_attrs:
+            _is_case2_build = True
+
+            for key,value in vars(data).items():
+                # Strip Unneeded Attrs/Methods, and Preserve Dunders if Required
+                if callable(value): continue
+                if (key.startswith('__') and key.endswith('__')): continue
+                if key.startswith(f'_{data.__name__}'): # dunder detection (name mangle)
+                    if (private_all_dunder_attrs) or (private_class_dunder_attrs):
+                        key = key.removeprefix(f'_{data.__name__}')
+                        _is_dunder_attr = True
+                    else: continue
+                if (key.startswith('_')) and not (_is_dunder_attr):
+                    if not (private_all_under_attrs or private_class_under_attrs):
+                        continue
+                _is_dunder_attr = False # reset dunder check for single under's
+
+                # Build Data
+                if __multiline_check(value) and indentation_on:
+                    value = __cleanformat(value, indent_level)
+                    __build_data_output += f'{key} {__assignment_operators[0]} {value}\n'
+                else: __build_data_output += f'{key} {__assignment_operators[0]} {repr(value)}\n'
+        
+        # Default Case 3 - Init Attrs or Class Attrs, or both
+        else:
+            _is_case3_build = True
+
+            # First: Init Attrs
+            if '__init__' not in vars(data):
+                for key,value in vars(data).items():
+                    # Strip Unneeded Attrs/Methods, and Preserve Dunders if Required
+                    if callable(value): continue
+                    if key.startswith(f'_{type(data).__name__}'): # dunder detection (name mangle)
+                        if (private_all_dunder_attrs) or (private_init_dunder_attrs):
+                            key = key.removeprefix(f'_{type(data).__name__}')
+                            _is_dunder_attr = True
+                        else: continue
+                    if (key.startswith('_')) and not (_is_dunder_attr):
+                        if not (private_all_under_attrs or private_init_under_attrs):
+                            continue
+                    _is_dunder_attr = False # reset dunder check for single under's
+                
+                    # Build Data - Init Attrs
+                    if __multiline_check(value) and indentation_on:
+                        value = __cleanformat(value, indent_level)
+                        __build_data_output_init += f'{key} {__assignment_operators[0]} {value}\n'
+                    else: __build_data_output_init += f'{key} {__assignment_operators[0]} {repr(value)}\n'
+                
+            # Last: Class Attrs
+            if class_attrs:
+                for key,value in vars(type(data)).items():
+                    # Strip Unneeded Attrs/Methods, and Preserve Dunders if Required
+                    if callable(value): continue
+                    if (key.startswith('__') and key.endswith('__')): continue
+                    if key.startswith(f'_{type(data).__name__}'): # dunder detection (name mangle)
+                        if (private_all_dunder_attrs) or (private_class_dunder_attrs):
+                            key = key.removeprefix(f'_{type(data).__name__}')
+                            _is_dunder_attr = True
+                        else: continue
+                    if (key.startswith('_')) and not (_is_dunder_attr):
+                        if not (private_all_under_attrs or private_class_under_attrs):
+                            continue
+                    _is_dunder_attr = False # reset dunder check for single under's
+                    
+                    # Build Data - Class Attrs
+                    if __multiline_check(value) and indentation_on:
+                        value = __cleanformat(value, indent_level)
+                        __build_data_output_class += f'{key} {__assignment_operators[0]} {value}\n'
+                    else: __build_data_output_class += f'{key} {__assignment_operators[0]} {repr(value)}\n'
+
+        # Structure Build
+        if _is_case1_build: pass
+        elif _is_case2_build: pass
+        elif _is_case3_build:
+            if not class_attrs:
+                __build_data_output = __build_data_output_init 
+            else: 
+                __build_data_output = '\n'.join([
+                                            _class_attr_header,
+                                            __build_data_output_class,
+                                            _init_attr_header,
+                                            __build_data_output_init,
+                                        ])
         # Strip Last \n Char
         __build_data_output = __build_data_output.rstrip()
 
