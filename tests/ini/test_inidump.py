ea1d67c5b41f038407314e763bc97193964565fe
aaronater10 <dev_admin@dunnts.com>
2023-03-24 22:50:47 -0500

    add/update tests for ini

--- tests/ini/test_inidump.py
+++ tests/ini/test_inidump.py
@@ -10,8 +10,8 @@ file_delay_timer = 0.5
 ################################################################
 # TESTS
 
-# 1. INI Data Export - Exporting ini file data and test attributes
-def test1_ini_file_export():
+# 1. INI Data Dump - Exporting ini file data and test attributes
+def test1_inidump_export():
     filename = '1_export_file_data.ini'
     filepath = test_file_path + filename
     ini_data = {
@@ -43,6 +43,85 @@ def test1_ini_file_export():
     for section,_ in ini_data.items():
         assert str(ini_data[section]['key']) == file_import.get(section, 'key')
         assert isinstance(file_import.get(section, 'key'), str)
+
+    # Remove Test File
+    time.sleep(file_delay_timer)
+    try: remove(filepath)
+    except: pass
+
+
+# 2. INI Data Append - Appending ini data to ini file and test data
+def test2_inidump_append():
+    filename = '2_append_file_data.ini'
+    filepath = test_file_path + filename
+    
+    # Remove Any Existing Test File
+    try: remove(filepath)
+    except: pass
+    time.sleep(file_delay_timer)
+
+    # Test not exist, create file by appending data, check if file exist, then test data
+    assert not path.exists(filepath)
+    append_times = 3
+    sections_match = []
+    for append_num in range(append_times):
+        section_name = f'str_data_{append_num}'
+        ini_data = {section_name: {'key': 'data'}}
+        ini_data_build = maci.inibuildauto(ini_data)
+        maci.inidump(filepath, ini_data_build, append=True)
+        sections_match.append(section_name)
+
+    assert path.exists(filepath)
+    file_import = maci.iniload(filepath)
+
+    # Test Sections Match
+    assert (file_import.sections() == sections_match)
+
+    # Test Section Data from File Load
+    for section in sections_match:
+        assert 'data' == file_import.get(section, 'key')
+
+    # Remove Test File
+    time.sleep(file_delay_timer)
+    try: remove(filepath)
+    except: pass
+
+
+# 3. Encoding - Test some common encoding types
+def test3_inidump_and_iniload_encodings():
+    filename = '3_ini_encoding.ini'
+    filepath = test_file_path + filename
+    encodings_to_test = {
+        'utf-8',
+        'utf-16',
+        'utf-32',
+        'ascii',
+        'iso-8859-1',
+        'cp1252',
+    }
+    
+    # Remove Any Existing Test File
+    try: remove(filepath)
+    except: pass
+    time.sleep(file_delay_timer)
+    assert not path.exists(filepath)
+
+    # Setup Ini Data
+    section_name = 'str_data'
+    ini_data = {section_name: {'key': 'data'}}
+    ini_data_build = maci.inibuildauto(ini_data)
+
+    # Test Dump and Load with Various Encodings
+    for encoding in encodings_to_test:
+        maci.inidump(filepath, ini_data_build, encoding=encoding)
+        time.sleep(file_delay_timer)
+        file_import = maci.iniload(filepath, encoding=encoding)
+
+        # Test Section Match
+        assert (file_import.sections() == [section_name])
+
+        # Test Section Data from File Load
+        assert 'data' == file_import.get(section_name, 'key')
 
     # Remove Test File
     time.sleep(file_delay_timer)
