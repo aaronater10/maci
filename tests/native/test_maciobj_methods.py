add85fcaa46f5abe9a85fa43804de96827455267
aaronater10 <dev_admin@dunnts.com>
2023-05-03 17:59:15 -0500

    update maciobj method tests

--- tests/native/test_maciobj_methods.py
+++ tests/native/test_maciobj_methods.py
@@ -52,6 +52,15 @@ def test1_maciobj_methods_glyph_integrity():
     assert file_import.map_lock == 'data'
     assert file_import.map_hard_lock == 'data'
 
+    assert 'norm_data' in file_import.get_parent_maps()
+    assert 'lock_data' in file_import.get_parent_maps()
+    assert 'hard_lock_data' in file_import.get_parent_maps()
+    assert 'map_data' in file_import.get_child_maps()
+    assert 'lock_data' in file_import.get_locked_list()
+    assert 'hard_lock_data' in file_import.get_hard_locked_list()
+    assert 'map_lock' in file_import.get_child_maps() and 'map_lock' in file_import.get_locked_list()
+    assert 'map_hard_lock' in file_import.get_child_maps() and 'map_hard_lock' in file_import.get_hard_locked_list()
+
     # Remove Test File
     time.sleep(file_delay_timer)
     try: remove(filepath)
@@ -135,7 +144,7 @@ def test4_maciobj_methods_parent_child_large_map_and_sources():
 
 
 # 5. MaciDataObj - MAP: Test Parent Chain Data and Structure
-def test5_maciobj_methods_parent_chains():
+def test5_maciobj_methods_map_parent_chains():
     # Build Data
     maci_data = maci.build()
     maci_data.norm_data1 = 'data1'
@@ -189,3 +198,99 @@ def test5_maciobj_methods_parent_chains():
     assert maci_data.get_parent_map_chains(dup_link_check=False) == {'norm_data1': ['norm_data1', 'map_data2'], 'norm_data2': ['norm_data2', 'map_data4']}
     assert maci_data.get_parent_map_chains('norm_data1', dup_link_check=False) == ['norm_data1', 'map_data2']
     assert maci_data.get_parent_map_chains('norm_data2', dup_link_check=False) == ['norm_data2', 'map_data4']
+
+
+# 6. MaciDataObj - UNMAP: Test Map being Unmapped Directly and Indirectly
+def test6_maciobj_methods_map_data_followed():
+    # Build Data
+    maci_data = maci.build()
+    maci_data.norm_data1 = 'ring_data'
+    maci_data.map_data1 = None
+    maci_data.map_data2 = None
+    maci_data.map_data3 = None
+
+    maci_data.norm_data2 = 'chain_data'
+    maci_data.map_data4 = None
+    maci_data.map_data5 = None
+    maci_data.map_data6 = None
+
+    # Setup Attrs: Ring Style, then Chain Style
+
+    # RING
+    maci_data.map_attr('map_data1', 'norm_data1')
+    maci_data.map_attr('map_data2', 'norm_data1')
+    maci_data.map_attr('map_data3', 'norm_data1')
+
+    # CHAIN
+    maci_data.map_attr('map_data4', 'norm_data2')
+    maci_data.map_attr('map_data5', 'map_data4')
+    maci_data.map_attr('map_data6', 'map_data5')
+
+    # Test Map Data, and Ring Data & Chain Data is Followed by Parent Re-Assignment
+    assert maci_data.map_data1 == 'ring_data'
+    assert maci_data.map_data2 == 'ring_data'
+    assert maci_data.map_data3 == 'ring_data'
+    assert maci_data.map_data4 == 'chain_data'
+    assert maci_data.map_data5 == 'chain_data'
+    assert maci_data.map_data6 == 'chain_data'
+
+    # RING: Parent Data Changed "norm_data1"
+    maci_data.norm_data1 = 'ring_data_changed'
+    assert maci_data.map_data1 == 'ring_data_changed'
+    assert maci_data.map_data2 == 'ring_data_changed'
+    assert maci_data.map_data3 == 'ring_data_changed'
+
+    # CHAIN: Parent Data Changed "norm_data2"
+    maci_data.norm_data2 = 'chain_data_changed'
+    assert maci_data.map_data4 == 'chain_data_changed'
+    assert maci_data.map_data5 == 'chain_data_changed'
+    assert maci_data.map_data6 == 'chain_data_changed'
+
+
+# 7. MaciDataObj - UNMAP: Test Map being Unmapped Directly and Indirectly
+def test7_maciobj_methods_unmap_direct_indirect():
+    # Build Data
+    maci_data = maci.build()
+    maci_data.norm_data1 = 'data1'
+    maci_data.map_data1 = None
+    maci_data.map_data2 = None
+
+    maci_data.norm_data2 = 'data2'
+    maci_data.map_data3 = None
+    maci_data.map_data4 = None
+
+    # Setup Attrs
+    maci_data.map_attr('map_data1', 'norm_data1')
+    maci_data.map_attr('map_data2', 'norm_data1')
+
+    maci_data.map_attr('map_data3', 'norm_data2')
+    maci_data.map_attr('map_data4', 'norm_data2')
+
+    # Test Map Data and Unmap
+    assert maci_data.map_data1 == 'data1'
+    assert maci_data.map_data2 == 'data1'
+    assert maci_data.map_data3 == 'data2'
+    assert maci_data.map_data4 == 'data2'
+    assert 'map_data1' in maci_data.get_child_maps()
+    assert 'map_data2' in maci_data.get_child_maps()
+    assert 'map_data3' in maci_data.get_child_maps()
+    assert 'map_data4' in maci_data.get_child_maps()
+
+    # UNMAP Child: Direct "map_data1" - Parent Map Still Intact
+    maci_data.unmap_attr('map_data1')
+    assert 'map_data1' not in maci_data.get_child_maps()
+    assert 'norm_data1' in maci_data.get_parent_maps()
+
+    # UNMAP Child: InDirect "map_data2" (From Value Re-Assignment)
+    maci_data.map_data2 = None
+    assert 'map_data2' not in maci_data.get_child_maps()
+
+    # UNMAP Parent: Direct "norm_data2" - Parent Map Still Intact
+    maci_data.unmap_attr('norm_data2')
+    assert 'map_data3' not in maci_data.get_child_maps()
+    assert 'map_data4' not in maci_data.get_child_maps()
+
+    # Test Maps Empty
+    assert maci_data.get_parent_maps() ==  {}
+    assert maci_data.get_parent_map_chains() ==  {}
+    assert maci_data.get_child_maps() ==  {}
