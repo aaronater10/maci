3af9e3d1f9cde0db9ec400201ea894710dc847bf
aaronater10 <dev_admin@dunnts.com>
2023-05-27 16:09:41 -0500

    update maciobj method exc tests

---
+++ tests/native/test_maciobj_method_exceptions.py
@@ -0,0 +1,183 @@
+# exceptions: MaciDataObj Method - Tests
+from src import maci
+import pytest
+
+
+################################################################
+# TESTS
+
+# 1. get_parent_map_chains - Type Checks
+def test1_exc_maciobj_method_get_parent_map_chains_types():
+    # Build Data
+    maci_data = maci.build()    
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.get_parent_map_chains(parent_attr=1.0)
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.get_parent_map_chains(parent_attr="", dup_link_check=1.0)    
+
+
+# 2. get_parent_map_chains - Unsupported Options or Data
+def test2_exc_maciobj_method_get_parent_map_chains_opts_data():
+    # Build Data
+    maci_data = maci.build() 
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.get_parent_map_chains(parent_attr="") 
+
+
+
+# 1. hard_lock_attr - Type Checks
+def test1_exc_maciobj_method_hard_lock_attr_types():
+    # Build Data
+    maci_data = maci.build()
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.hard_lock_attr(attr_name=1.0)  
+
+
+# 2. hard_lock_attr - Unsupported Options or Data
+def test2_exc_maciobj_method_hard_lock_attr_opts_data():
+    # Build Data
+    maci_data = maci.build() 
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.hard_lock_attr(attr_name="")
+
+
+
+# 1. lock_attr - Type Checks
+def test1_exc_maciobj_method_lock_attr_types():
+    # Build Data
+    maci_data = maci.build()
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.lock_attr(attr_name=1.0)  
+
+
+# 2. lock_attr - Unsupported Options or Data
+def test2_exc_maciobj_method_lock_attr_opts_data():
+    # Build Data
+    maci_data = maci.build() 
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.lock_attr(attr_name="")
+
+
+
+# 1. is_child_map - Type Checks
+def test1_exc_maciobj_method_is_child_map_types():
+    # Build Data
+    maci_data = maci.build()
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.is_child_map(attr_name=1.0)  
+
+
+# 2. is_child_map - Unsupported Options or Data
+def test2_exc_maciobj_method_is_child_map_opts_data():
+    # Build Data
+    maci_data = maci.build() 
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.is_child_map(attr_name="")
+
+
+
+# 1. is_parent_map - Type Checks
+def test1_exc_maciobj_method_is_parent_map_types():
+    # Build Data
+    maci_data = maci.build()
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.is_parent_map(attr_name=1.0)  
+
+
+# 2. is_parent_map - Unsupported Options or Data
+def test2_exc_maciobj_method_is_parent_map_opts_data():
+    # Build Data
+    maci_data = maci.build() 
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.is_parent_map(attr_name="")
+
+
+
+# 1. map_attr - Type Checks
+def test1_exc_maciobj_method_map_attr_types():
+    # Build Data
+    maci_data = maci.build()
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.map_attr(child_attr=1.0, parent_attr="")
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.map_attr(child_attr="", parent_attr=1.0)  
+
+
+# 2. map_attr - Unsupported Options or Data
+def test2_exc_maciobj_method_map_attr_opts_data():
+    # Build Data
+    maci_data = maci.build()
+    maci_data.child = 1
+    maci_data.parent = 1
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.map_attr(child_attr="", parent_attr="parent")
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.map_attr(child_attr="child", parent_attr="")
+
+
+
+# 1. unlock_attr - Type Checks
+def test1_exc_maciobj_method_unlock_attr_types():
+    # Build Data
+    maci_data = maci.build()
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.unlock_attr(attr_name=1.0)  
+
+
+# 2. unlock_attr - Unsupported Options or Data
+def test2_exc_maciobj_method_unlock_attr_opts_data():
+    # Build Data
+    maci_data = maci.build() 
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.unlock_attr(attr_name="")
+
+
+
+# 1. unmap_attr - Type Checks
+def test1_exc_maciobj_method_unmap_attr_types():
+    # Build Data
+    maci_data = maci.build()
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.unmap_attr(attr_name=1.0)
+
+
+# 2. unmap_attr - Unsupported Options or Data
+def test2_exc_maciobj_method_unmap_attr_opts_data():
+    # Build Data
+    maci_data = maci.build()
+    maci_data.child = 1
+    maci_data.parent = 1
+
+    # Tests
+    with pytest.raises(maci.error.GeneralError):
+        maci_data.unmap_attr(attr_name="")
