fd59f66f0e3d0c4168f47259255104c2fe3f23a8
aaronater10 <dev_admin@dunnts.com>
2023-06-17 15:09:00 -0500

    update xml build names

--- src/maci/_xml/xmlbuildmanual.py
+++ src/maci/_xml/xmlbuildmanual.py
@@ -1,17 +1,17 @@
 # xmlbuildmanual
 #########################################################################################################
 # Imports
-import xml.etree.ElementTree as __xml_etree
+import xml.etree.ElementTree as _xml_etree
 
 #########################################################################################################
 # Build manual xml data
-def xmlbuildmanual() -> __xml_etree:
+def xmlbuildmanual() -> _xml_etree:
     """
-    Returns an empty xml ElementTree obj to build/work with xml data
+    Returns a xml ElementTree module object to build/work with xml data
     
     Assign the output to var
 
     This is using the native xml library via etree shipped with the python standard library.
     For more information on the xml.etree api, visit: https://docs.python.org/3/library/xml.etree.elementtree.html#module-xml.etree.ElementTree
     """
-    return __xml_etree
+    return _xml_etree
