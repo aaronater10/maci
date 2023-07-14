05048ed378c622f8e8bfc6cbb04bb5b4c9ab49b3
aaronater10 <dev_admin@dunnts.com>
2023-07-13 22:03:01 -0500

    increment revision number

--- src/maci/__init__.py
+++ src/maci/__init__.py
@@ -3,13 +3,13 @@ maci - by aaronater10
 
 A Pythonic Configuration Language & Thin Wrapper Library
 
-Version 0.4.0
+Version 0.5.0
 
 Tutorials and docs: https://docs.macilib.org
 
 Source: https://github.com/aaronater10/maci
 """
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 #########################################################################################################
 # Imports
